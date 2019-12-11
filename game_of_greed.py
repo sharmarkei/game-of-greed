5b7a30ea3e51d3b211b228d7a9c0c46b02892167
sharmarke ibrahim <smarky292@gmail.com>
2019-12-11 09:31:36 -0800

    forgot to push last night

--- game_of_greed.py
+++ game_of_greed.py
@@ -1,57 +1,155 @@
-import collections
-# Today is all about tackling the highest risk features - scoring and the game flow.
-# Define a Game class.
+import random
+from collections import Counter
 
-class GameOfGreed:
-    
+
+class Game:
     def __init__(self, print_func=print, input_func=input):
         self._print = print_func
         self._input = input_func
 
-    # Handle calculating score for dice roll
-    def calculate_score(self, current_dice_roll=(2,2,4,4,6,6)):       
-        tallie_of_dice = collections.Counter(current_dice_roll)
-        roll_score = 0
-        for num in (1,2,3,4,5,6):
-            a = '{}{}'.format(num, tallie_of_dice[num])
-            if len(tallie_of_dice) == 6: #straight • six/six unique numbers
-                roll_score += 1500
-                return roll_score
-            if len(tallie_of_dice.values()) == 3 and set(tallie_of_dice.values()) == {2}: # three pairs
-                roll_score += 1500
-                return roll_score              
-            elif a[0] == '1': # ones
-                if a[1] < '3':
-                    roll_score += 100*int(a[1])
+    def roll_dice(self):
+
+        '''
+        Simulates rolling 6 dice and appends those values into the below dice object. 
+        '''
+
+        dice = []
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+        dice.append(random.randint(1, 6))
+
+        return dice
+
+    # Calculate the score
+
+    def score_game(self, dice):
+        '''
+        Every possibiblity of scoring has its own instance. Starting with the most rare case to the most common case to avoid any miscalculations.
+        '''
+        total = 0
+        cnt = Counter(dice)
+
+        '''
+        What the counter is doing is adding the dice to a dictionary where it is then adding up the values of each value. So say you roll 4 2s, the dictionary under the key of 2 will be 4 as a value.
+        '''
+
+        one = 100
+        five = 50
+
+        for key, value in cnt.items():
+
+            # Straight Score
+            if (
+                value == 1
+                and value == 2
+                and value == 3
+                and value == 4
+                and value == 5
+                and value == 6
+            ):
+                total += 1500
+                break
+
+            # One Score
+            if key == 1:
+                if value == 6:
+                    total += one * 40
+                elif value == 5:
+                    total += one * 30
+                elif value == 4:
+                    total += one * 20
+                elif value == 3:
+                    total += one * 10
                 else:
-                    roll_score += 1000*(int(a[1])-2)
-            elif a[0] == '5': # fives
-                if a[1] >= '3':
-                    roll_score += 100*int(a[0])*(int(a[1])-2)
-                else:    
-                    roll_score += 50*int(a[1])
-            elif a[1] >= '3': # 3 or more of a kind
-                roll_score += 100*int(a[0])*(int(a[1])-2)
-        # The output from calculate_score is an integer representing the roll’s score according to rules of game.
-        return roll_score
+                    total += value * one
+
+            # Two Score
+            if key == 2:
+                if value == 6:
+                    total += 800
+                elif value == 5:
+                    total += 600
+                elif value == 4:
+                    total += 400
+                elif value == 3:
+                    total += 200
+                else:
+                    total += 0
+
+            # Three Score
+            if key == 3:
+                if value == 6:
+                    total += 1200
+                elif value == 5:
+                    total += 900
+                elif value == 4:
+                    total += 600
+                elif value == 3:
+                    total += 300
+                else:
+                    total += 0
+
+            # Four Score
+            if key == 4:
+                if value == 6:
+                    total += 1600
+                elif value == 5:
+                    total += 1200
+                elif value == 4:
+                    total += 800
+                elif value == 3:
+                    total += 400
+                else:
+                    total += 0
+
+            # Five Score
+            if key == 5:
+                if value == 6:
+                    total += five * 40
+                elif value == 5:
+                    total += five * 30
+                elif value == 4:
+                    total += five * 20
+                elif value == 3:
+                    total += five * 10
+                else:
+                    total += value * five
+
+            # Six Score
+            if key == 6:
+                if value == 6:
+                    total += 2400
+                elif value == 5:
+                    total += 1800
+                elif value == 4:
+                    total += 1200
+                elif value == 3:
+                    total += 600
+                else:
+                    total += 0
+            
+        return total    
+
+        print(total)
 
     def play(self, user_response=None):
-        # Greet user by printing ‘Welcome to Game of Greed’
-        self._print('Welcome to Game of Greed')
 
-        # Prompt user with ‘Wanna play?’
-        response = self._input('Wanna play?')
+        '''
+        Prints a prompt in the beginning of terminal and prints greeting accordingly.
+        '''
+        
+        self._print("Welcome to Game of Greed!")
+        response = self._input("Wanna play? ")
 
-        # if user enters ‘y’ then print ‘Great! Check back tomorrow :D’
-        if response == 'y' or user_response == 'y':
-            self._print('Great! Check back tomorrow :D')
-
-        # if user enters anything else print ‘OK. Maybe another time’
+        if response == "y" or user_response == "y":
+            self._print("Great! Check back tomorrow!")
         else:
-            self._print('OK. Maybe another time')
+            self._print("OK. Maybe another time")
+
 
 if __name__ == "__main__":
-    game = GameOfGreed()
-
+    game = Game()
     game.play()
-    game.calculate_score()
